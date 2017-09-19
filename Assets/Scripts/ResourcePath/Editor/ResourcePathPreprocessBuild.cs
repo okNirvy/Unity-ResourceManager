de8c9b10410912536510304954e108215ec555dd
okNirvy <burutya108@yahoo.co.jp>
2017-09-19 23:27:49 +0900

    Add pre-process build(Unity5.6 or newer).

---
+++ Assets/Scripts/ResourcePath/Editor/ResourcePathPreprocessBuild.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_5_6_OR_NEWER
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Build;
+using System;
+using UnityEditor;
+
+public class ResourcePathPreprocessBuild : IPreprocessBuild
+{
+    public int callbackOrder {
+        get {
+            throw new NotImplementedException();
+        }
+    }
+
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        ResourcePath.instance.LoadXML();
+    }
+}
+#endif
